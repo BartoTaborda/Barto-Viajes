c94abb241e7b93e698e6cbd9f62c28850de0b1f3
bartotaborda <agustin-taborda@hotmail.com.ar>
2022-06-06 19:29:51 -0300

    Prueba 1

---
+++ js/slider.js
@@ -0,0 +1,38 @@
+(function () {
+    
+/* ----------- OBJETO CON LAS PROP DEL SLIDER ----------- */
+
+    var propSlider = {
+
+        slider: document.getElementById('slider'),
+        primerSlide: null,
+
+    }
+
+
+
+
+/* ----------- OBJETO CON LOS METODOS DEL SLIDER ----------- */
+
+    var metSlider = {
+
+        inicio: function () {
+            setInterval(metSlider.moverSlide, 2500)
+        },
+
+        moverSlide: function () {
+            propSlider.slider.style.transition = 'all 1.5s ease';
+            propSlider.slider.style.marginLeft = '-100%';
+
+            setTimeout(function () {
+                propSlider.primerSlide = propSlider.slider.firstElementChild;
+                propSlider.slider.appendChild(propSlider.primerSlide);
+                propSlider.slider.style.transition = 'unset';
+                propSlider.slider.style.marginLeft = '0%';
+            }, 1500);
+        }
+    }
+
+    metSlider.inicio();
+
+}())
